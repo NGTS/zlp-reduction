7bfca85982b62a62fac02c5c05287d18cc3288a9
Simon Walker <s.r.walker101@googlemail.com>
2015-06-08 11:43:38 +0100

    Run dark tests

--- test.sh
+++ test.sh
@@ -4,13 +4,14 @@ set -e
 
 OUTPUTDIR=${PWD}/output
 
-setup_environment() {
-    export PYTHONPATH=../zlp-script/scripts:$PYTHONPATH
-}
-
 setup_outputdir() {
     test -d $OUTPUTDIR && rm -rf $OUTPUTDIR
     mkdir -p $OUTPUTDIR
+}
+
+setup_environment() {
+    export PYTHONPATH=../zlp-script/scripts:$PYTHONPATH
+    setup_outputdir
 }
 
 test_create_bias() {
@@ -20,17 +21,25 @@ test_create_bias() {
     MBIAS=mbias.fits
 
     python bin/pipebias.py $BIASLIST $MBIAS $OUTPUTDIR
+
     FILENAME=$OUTPUTDIR/$MBIAS py.test -m bias testing
 }
 
-run_test() {
-    setup_outputdir
-    $1
+test_create_dark() {
+    DARKLIST=$TMPDIR/darklist.txt
+    find testdata/dark -name 'IMAGE*.bz2' > $DARKLIST
+
+    MDARK=mdark.fits
+
+    python bin/pipedark.py $DARKLIST $MBIAS $MDARK $OUTPUTDIR
+
+    FILENAME=$OUTPUTDIR/$MDARK py.test -m dark testing
 }
 
 main() {
     setup_environment
-    run_test test_create_bias
+    test_create_bias
+    test_create_dark
 }
 
 main
